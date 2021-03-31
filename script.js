eff10d57e1f853f03b7e585410456ac4ea966ef8
Andy Duarte <andyduarte@gmail.com>
2021-03-31 15:29:58 -0500

    results at the moment

--- script.js
+++ script.js
@@ -4,15 +4,33 @@ const MIN_DURATION = 2000;
 const MAX_DURATION = 5000;
 
 const snowflakesContainer = document.getElementById("snowflakes-container");
+const snowVsRain = document.getElementById("snow-vs-rain")
+const colorClicker = document.getElementById("click-color")
+const quantity = document.getElementById("quantity")
+let quantityAmount = 50
+setInterval(() => createSnowflake(), quantityAmount);
+switch(quantity){
+  case 0:
+    quantityAmount = 200
+  break;
+  case 1:
+    quantityAmount = 150
+  break;
+  case 2:
+    quantityAmount = 100
+  break;
+  case 3:
+    quantityAmount = 50
+  break;
+}
 
-setInterval(() => createSnowflake(), 50);
 
 function randint(lo, hi) {
   return Math.random() * (hi - lo) + lo;
 }
 
 function randomIcon() {
-  if (Math.random() < 0.5) {
+  if (Math.random() > Number(snowVsRain.value)) {
     return "fa-snowflake";
   } else {
     return "fa-tint";
@@ -26,6 +44,7 @@ function createSnowflake() {
   snowFlake.style.left = randint(0, 100) + "%";
   snowFlake.style.opacity = Math.random();
   snowFlake.style.fontSize = randint(MIN_SIZE, MAX_SIZE) + "px";
+  snowFlake.style.color = colorClicker.value;
 
   snowflakesContainer.appendChild(snowFlake);
 
